3946eb42b80e905d1579848a6ded49e438c3425d
Kimberly miller <135668982+Kmiller105@users.noreply.github.com>
2025-08-25 17:29:58 -0500

    uodate

---
+++ functions.js
@@ -0,0 +1,26 @@
+const name="Kim M";
+const youtuber ="Chaotic Designs";
+const myobject={
+    property1:"value",
+    property2:123,
+    property3: true
+};
+const value=document.querySelector('input[type=value"]');
+addEventListener("blur"),(event)=>{
+    onblur=(event)=>{
+
+    }
+}
+function logger(text)
+{
+    
+    const upperCased=text.logger();
+    console.log("Life Time 1");
+    console.log("Life Time 2"); 
+    console.log("Life Time 3");
+    console.log("Life Time 4");
+}
+logger(text);
+Object.defineProperty("value", 123,{value:"6"})
+
+
